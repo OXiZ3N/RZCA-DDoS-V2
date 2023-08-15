741900e08309994ec00c1853fe36435b36e5ae32
OXiZ3N <101161664+OXiZ3N@users.noreply.github.com>
2023-08-15 15:13:10 +0600

    rz.py

--- rz.py
+++ rz.py
@@ -712,20 +712,3 @@ CLEAR   ► CLEAR TERMINAL
                 pass
 
 
-def login():
-    clear()
-    user = "rzca"
-    passwd = "rzca"
-    username = input("⚡ Username: ")
-    password = getpass.getpass(prompt='⚡ Password: ')
-    if username != user or password != passwd:
-        print("")
-        print("⚡ Haizzz, you're so cute...")
-        sys.exit(1)
-    elif username == user and password == passwd:
-        print("⚡ Welcome to RZCA DDoS-V2")
-        time.sleep(0.3)
-        ascii_vro()
-        main()
-
-login()
